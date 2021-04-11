5e5bcf6078dd15dd2fbee98baffecfa2d148f745
XDD666 <35656085+CNAD666@users.noreply.github.com>
2021-04-11 12:11:33 +0800

    升级SmartDialog版本

--- lib/main.dart
+++ lib/main.dart
@@ -13,13 +13,25 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      initialRoute: RouteConfig.main,
-      getPages: RouteConfig.getPages,
-      builder: builder,
+    return MaterialApp(
+      home: Container(),
+      builder: (BuildContext context, Widget child) {
+        return FlutterSmartDialog(child: child);
+      },
     );
   }
 }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return GetMaterialApp(
+//       initialRoute: RouteConfig.main,
+//       getPages: RouteConfig.getPages,
+//       builder: builder,
+//     );
+//   }
+// }
 
 ///独立处理全局bloc
 Widget builder(BuildContext context, Widget child) {
