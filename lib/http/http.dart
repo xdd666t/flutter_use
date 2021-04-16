17f7bf02d7314f5eb5c918f42eeef78c6e656d7a
XDD666 <35656085+CNAD666@users.noreply.github.com>
2021-04-16 21:49:36 +0800

    处理Http请求超时问题

--- lib/http/http.dart
+++ lib/http/http.dart
@@ -1,22 +1,17 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_toolkit_easy/flutter_toolkit.dart';
-import 'package:flutter_use/bean/test/image_info_bean.dart';
 import 'package:flutter_use/views/dialog/easy/easy_dialog.dart';
 
 ///举例  todo：暂时还有问题
 testHttp() async {
   ///放到主入口初始化,必须
   Http.init();
-  Http.setHeaders({});
 
   Log.d('测试Http');
-  var query = {'return': 'json'};
-  var result = await Http.get(
-    'https://api.ixiaowai.cn/api/api.php',
-    queryParameters: query,
+  var result = await NetUtil.instance.get(
+    'https://www.wanandroid.com/banner/json',
   );
-  ImageInfoBean bean = ImageInfoBean().fromJson(result);
-  Log.d(bean.imgurl);
+  Log.i(result);
 }
 
 class Http {
