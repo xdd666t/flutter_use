c651db795545a91c0ce2d9eb5382f8a2fa8501df
XDD666 <xdd666@foxmail.com>
2021-06-25 15:48:02 +0800

    修复路由跳转错误

--- lib/module/example/getx/jump_page/jump_one/logic.dart
+++ lib/module/example/getx/jump_page/jump_one/logic.dart
@@ -7,7 +7,7 @@ class GetJumpOneLogic extends GetxController {
 
   ///跳转到跨页面
   void toJumpTwo() {
-    Get.toNamed(RouteConfig.getJumpOne, arguments: {'msg': '我是上个页面传递过来的数据'});
+    Get.toNamed(RouteConfig.getJumpTwo, arguments: {'msg': '我是上个页面传递过来的数据'});
   }
 
   ///跳转到跨页面
