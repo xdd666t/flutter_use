338369f2bfdb492faf103c9cc822039cf3135111
XDD666 <xdd666@foxmail.com>
2021-07-15 11:11:32 +0800

    调整初始化位置

--- lib/module/setting/logic.dart
+++ lib/module/setting/logic.dart
@@ -1,7 +1,16 @@
 import 'package:get/get.dart';
+import 'package:visibility_detector/visibility_detector.dart';
 
 import 'state.dart';
 
 class SettingLogic extends GetxController {
   final state = SettingState();
+
+  @override
+  void onInit() {
+    //回调在每一帧的末尾触发,避免widget未加载完产生的报错
+    VisibilityDetectorController.instance.updateInterval = Duration.zero;
+    super.onInit();
+  }
+
 }
--- lib/module/setting/state.dart
+++ lib/module/setting/state.dart
@@ -1,9 +1,7 @@
-import 'package:visibility_detector/visibility_detector.dart';
+
 
 class SettingState {
   SettingState() {
-    //回调在每一帧的末尾触发,避免widget未加载完产生的报错
-    VisibilityDetectorController.instance.updateInterval = Duration.zero;
     ///Initialize variables
   }
 }
