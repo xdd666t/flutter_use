320a6c1941c256b7cacb494f1791814aa6bd8ca6
XDD666 <xdd666@foxmail.com>
2021-09-23 15:16:59 +0800

    简化定义

--- lib/module/example/getx/counter_high_get/logic.dart
+++ lib/module/example/getx/counter_high_get/logic.dart
@@ -2,7 +2,7 @@ import 'package:flutter_use/module/example/getx/counter_high_get/state.dart';
 import 'package:get/get.dart';
 
 class GetCounterHighLogic extends GetxController {
-  final state = GetCounterHighState();
+  final GetCounterHighState state = GetCounterHighState();
 
   ///自增
   void increase() {
