2228720f1cbb0bd669a95c6c7aef12a360d9db75
小呆呆666 <xdd666t@gmail.com>
2023-04-08 16:53:29 +0800

    update web effect

--- docs/web/main.dart.js_11.part.js
+++
@@ -1,6 +0,0 @@
-self.$__dart_deferred_initializers__=self.$__dart_deferred_initializers__||Object.create(null)
-$__dart_deferred_initializers__.current=function(a,b,c,$){var A,B
-A=c[94]
-B=c[0]
-var z=a.updateTypes([]);(function constants(){A.ov=new B.bs(58727,"MaterialIcons",null,!1)})()}
-$__dart_deferred_initializers__["hGsOCSsv9EEwl7a132E3IeAhhxY="] = $__dart_deferred_initializers__.current
