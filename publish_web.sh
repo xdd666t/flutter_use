93d48baad5383b7f97e72f4764b6ea124e739f9e
小呆呆666 <xdd666t@gmail.com>
2023-04-22 15:52:58 +0800

    update web effect

--- publish_web.sh
+++ publish_web.sh
@@ -1,19 +1,27 @@
 # 代理终端
-export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890 \
 # flutter build web --web-renderer canvaskit
 # flutter build web --web-renderer html
-# 去掉pwa支持, dart2js压缩级别
-flutter clean && flutter pub get \
-&& flutter build web --web-renderer canvaskit --pwa-strategy none  --dart2js-optimization O1 \
+# 去掉pwa支持
+# dart2js压缩级别 (flutter默认的是O4优化级别)
+#  -O0: 禁用许多优化。
+#  -O1: 启用默认优化(仅是dart2js该命令的默认级别)
+#  -O2: 在-O1优化基础上,尊重语言语义且对所有程序安全的其他优化（例如缩小）
+#    备注：使用-O2,使用开发JavaScript编译器编译时,类型的字符串表示不再与Dart VM中的字符串表示相同
+#  -O3: 在-O2优化基础上,并省略隐式类型检查。
+#   注意：省略类型检查可能会导致您的应用程序因类型错误而崩溃
+#  -O4: 在-O3优化基础上,启用更积极的优化
+#   注意：-O4优化容易受到输入数据变化的影响,在依赖-O4之前,需测试用户输入中的边缘情况
 # 去掉未使用icons
 #  - flutter build apk
 #  - 使用apk资源产物, 替换web资源产物
-flutter build apk \
+# 编译出产物移到目标文件位置
+#export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890 \
+#&& flutter clean && flutter pub get \
+flutter build web --web-renderer canvaskit --pwa-strategy none  --dart2js-optimization O1 \
+&& flutter build apk \
 && cp -r ./build/app/intermediates/flutter/release/flutter_assets/ ./build/web/assets/ \
-# 编译出产物移到目标为止
-cp -r ./build/web/ ./docs/web/ \
-# push
-git add . && git commit -m "update web effect" && git push
+&& cp -r ./build/web/ ./docs/web/ \
+&& git add . && git commit -m "update web effect" && git push
 
 # macos执行该命令: bash publish_web.sh
 
