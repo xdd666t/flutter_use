32842f42d9bc4946d278311d3bea7c6205b5ccfb
小呆呆666 <xdd666t@gmail.com>
2023-04-22 15:58:08 +0800

    update web effect

--- publish_web.sh
+++ publish_web.sh
@@ -1,4 +1,3 @@
-# 代理终端
 # flutter build web --web-renderer canvaskit
 # flutter build web --web-renderer html
 # 去掉pwa支持
@@ -15,12 +14,13 @@
 #  - flutter build apk
 #  - 使用apk资源产物, 替换web资源产物
 # 编译出产物移到目标文件位置
-#export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890 \
-#&& flutter clean && flutter pub get \
-flutter build web --web-renderer canvaskit --pwa-strategy none  --dart2js-optimization O1 \
+# 代理终端, push到github
+flutter clean && flutter pub get \
+&& flutter build web --web-renderer canvaskit --pwa-strategy none --dart2js-optimization O1 \
 && flutter build apk \
 && cp -r ./build/app/intermediates/flutter/release/flutter_assets/ ./build/web/assets/ \
 && cp -r ./build/web/ ./docs/web/ \
+&& export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890 \
 && git add . && git commit -m "update web effect" && git push
 
 # macos执行该命令: bash publish_web.sh
