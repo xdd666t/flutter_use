3e831adf1f299d5e38122f36e9a2a95484b9a4c6
小呆呆666 <xdd666@foxmail.com>
2024-07-20 15:54:21 +0800

    调整android文件夹下生成文件

---
+++ android/app/src/main/kotlin/com/example/flutter_use/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_use
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/tao/flutter_use/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.tao.flutter_use
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
